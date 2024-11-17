9b0cf89e21107fe1b191b537326865f8e5d65b29
“harrysokool” <harry1232003@icloud.com>
2024-11-16 16:27:22 -0800

    update

--- backend/lambdas/textAnalysis/handler.py
+++ backend/lambdas/textAnalysis/handler.py
@@ -1,9 +1,9 @@
 import json
-import textAnalysis
+import text_analysis
 
 # need to change the model and also make it relate to our topic (emotion analysis)
 def lambda_handler(event, context):
-    result = textAnalysis.sentiment_analysis(event['text'])
+    result = text_analysis.sentiment_analysis(event['text'])
     
     return {
         'statusCode': 200,
