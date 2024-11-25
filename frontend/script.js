bb70930e810ed69e984a30ffba0c27dc7aa98b44
“harrysokool” <harry1232003@icloud.com>
2024-11-24 22:07:52 -0800

    able to upload files from frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
+// see if the user is logged in
 document.addEventListener("DOMContentLoaded", () => {
   const isLoggedIn = sessionStorage.getItem("isLoggedIn");
 
-  // Select the sections to show or hide
   const uploadSection = document.querySelector(".upload");
   const resultsSection = document.querySelector(".results");
 
@@ -16,9 +16,62 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-document.getElementById("logoutButton").addEventListener("click", () => {
-  sessionStorage.setItem("isLoggedIn", "false");
+// Log out the user
+// document.getElementById("logoutButton").addEventListener("click", () => {
+//   sessionStorage.setItem("isLoggedIn", "false");
 
-  alert("You have been logged out.");
-  window.location.href = "loginPage/loginPage.html";
-});
+//   alert("You have been logged out.");
+//   window.location.href = "loginPage/loginPage.html";
+// });
+
+// Handle form submission
+document
+  .getElementById("uploadForm")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const fileInput = document.getElementById("fileInput");
+    const file = fileInput.files[0];
+
+    if (!file) {
+      alert("Please choose a file to upload.");
+      return;
+    }
+
+    if (file.size > 10 * 1024 * 1024) {
+      alert("File size exceeds the 10MB limit.");
+      return;
+    }
+
+    const endpoint = `https://2de7lmjom7.execute-api.ca-central-1.amazonaws.com/dev2/prj-text-json-bucket/${file.name}`;
+
+    try {
+      const response = await fetch(endpoint, {
+        method: "PUT",
+        headers: {
+          "Content-Type": file.type,
+        },
+        body: file,
+      });
+
+      if (response.ok) {
+        const uploadResponse = document.querySelector(".uploadMSG");
+        uploadResponse.style.display = "inline-block";
+        document.getElementById(
+          "uploadMessage"
+        ).textContent = `File "${file.name}" uploaded successfully!`;
+        document.getElementById("uploadMessage").style.color = "green";
+      } else {
+        const errorMessage = await response.text();
+        document.getElementById(
+          "uploadMessage"
+        ).textContent = `Failed to upload file: ${errorMessage}`;
+        document.getElementById("uploadMessage").style.color = "red";
+      }
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      document.getElementById("uploadMessage").textContent =
+        "Error uploading file. Please try again.";
+      document.getElementById("uploadMessage").style.color = "red";
+    }
+  });
